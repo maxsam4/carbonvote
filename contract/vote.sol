a7cfbc36481614cf316da50584e0413296f12fde
Daniel Lv <lgn21st@gmail.com>
2017-01-20 19:45:03 +0800

    Update contract and deployed addresses

--- contract/vote.sol
+++ contract/vote.sol
@@ -1,14 +1,13 @@
-pragma solidity ^0.4.8;
-
 contract Vote {
     event LogVote(address indexed addr);
 
-    function() payable {
+    function() {
         LogVote(msg.sender);
 
         if (msg.value > 0) {
-            if (!msg.sender.send(msg.value))
+            if (!msg.sender.send(msg.value)) {
                 throw;
+            }
         }
     }
 }
