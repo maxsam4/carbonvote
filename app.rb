3e4f0dcdc2619ecc66f73a277aad906d4aaa8aab
Daniel Lv <lgn21st@gmail.com>
2017-01-20 17:28:01 +0800

    Web UI for Carbonvote

--- app.rb
+++ app.rb
@@ -1,7 +1,57 @@
 require 'sinatra/base'
+require "sinatra/reloader"
+require "sinatra/config_file"
+require "sinatra/json"
+
+require_relative 'lib/Carbonvote'
 
 class App < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  register Sinatra::ConfigFile
+  config_file 'settings.yml'
+
+  helpers do
+    def redis
+      @redis ||= Redis.new(url: settings.redis_url)
+    end
+
+    def last_block
+      redis.get('processed-block-number') || 0
+    end
+
+    def get_amount(name)
+      key = settings.contract_addresses[name]
+      (redis.get("#{key}-amount") || 0).to_f
+    end
+
+    def yes_vote_amount
+      [
+        get_amount(:yes_contract_1),
+        get_amount(:yes_contract_2),
+        get_amount(:yes_contract_3),
+        get_amount(:yes_contract_4),
+        get_amount(:yes_contract_5)
+      ].sum.round(4)
+    end
+
+    def no_vote_amount
+      get_amount(:no_contract).round(4)
+    end
+  end
+
   get '/' do
-    erb :index
+    erb :index, locals: {
+      settings: settings,
+      last_block: last_block,
+      no_vote_amount: no_vote_amount,
+      yes_vote_amount: yes_vote_amount
+    }
+  end
+
+  get '/vote' do
+    json yes: yes_vote_amount, no: no_vote_amount
   end
 end
---
+++ public/javscripts/index.js
@@ -0,0 +1,76 @@
+function createChart(yes, no) {
+  new Highcharts.Chart({
+    chart: {
+      renderTo: 'CarbonvoteChart',
+      plotBackgroundColor: null,
+      plotBorderWidth: null,
+      plotShadow: false,
+      type: 'pie'
+    },
+    credits: {
+      enabled: false
+    },
+    title: {
+      text: 'Vote Status'
+    },
+    tooltip: {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+        cursor: 'pointer',
+        dataLabels: {
+          enabled: false
+        },
+        showInLegend: true
+      }
+    },
+    series: [{
+      name: 'Brands',
+      colorByPoint: true,
+      data: [{
+        name: 'YES',
+        y: yes
+      }, {
+        name: 'NO',
+        y: no
+      }]
+    }],
+    colors: ['#99CC66', '#FF6666'],
+  })
+}
+
+function ajaxLoad(url, callback) {
+  var xmlhttp = new XMLHttpRequest();
+
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
+      if (xmlhttp.status == 200) {
+        callback(xmlhttp.responseText)
+      }
+      else if (xmlhttp.status == 400) {
+        console.log('There was an error 400');
+      }
+      else {
+        console.log('something else other than 200 was returned');
+      }
+    }
+  }
+
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send();
+}
+
+(function() {
+  ajaxLoad('/vote', function(res) {
+    var data = JSON.parse(res)
+    var yesVote = Number(data.yes)
+    var noVote  = Number(data.no)
+
+    var yes = Number(((yesVote / (yesVote + noVote)) * 100).toFixed())
+    var no = 100 - yes
+
+    createChart(yes, no)
+  })
+})()
